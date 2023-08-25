9fb7291c875fa9277c5ed41691698ec5f176eaae
Deepak Naidu <devdnn@users.noreply.github.com>
2023-08-25 21:33:01 +0000

    updated setup to create .dotfiles from git

--- setup.sh
+++ setup.sh
@@ -8,22 +8,20 @@ is_executable() {
   type "$1" > /dev/null 2>&1
 }
 
-# Basic file system setup
-mkdir -p $HOME/work/_code/git
-
 SOURCE="https://github.com/devdnn/macos-setup"
-SOURCECODE_LOCATION="$HOME/work/_code/git"
-TARBALL="$SOURCE/tarball/main"
-TARGET="$HOME/.dotfiles"
+TARGET="$HOME/.macos-setup"
+DOTFILES_FOLDER="$TARGET/dotfiles"
+
+# Basic file system setup
+rm -rf $TARGET
+mkdir -p $TARGET
 
 if is_executable "git"; then
-  CMD="git clone $SOURCE $SOURCECODE_LOCATION"
-  cp -r $SOURCECODE_LOCATION/macos-setup/dotfiles $HOME/.dotfiles
+  CMD="git clone $SOURCE $TARGET"
+  eval $CMD
 fi
 
-
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 curl -L https://raw.githubusercontent.com/devdnn/macos-setup/main/scripts/core-fonts.sh | sh
 
@@ -33,20 +31,18 @@ curl -L https://raw.githubusercontent.com/devdnn/macos-setup/main/scripts/homebr
 
 curl -L https://raw.githubusercontent.com/devdnn/macos-setup/main/scripts/macos-install.sh | sh
 
-
-
 # SSH config
-stow ssh -d $TARGET -t $HOME
+stow ssh -d $DOTFILES_FOLDER -t $HOME
 
 # stow bash -t $HOME
 rm $HOME/.zshrc
-stow zsh --d $TARGET -t $HOME
+stow zsh --d $DOTFILES_FOLDER -t $HOME
 
 # stow vscode -t $HOME
 # ln -s "$HOME/work/git/stow-dotfiles/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
 
 # git
-stow git -d $TARGET -t $HOME
+stow git -d $DOTFILES_FOLDER -t $HOME
 git config --global core.excludesfile $HOME/.gitignore
 
 # Node.js setup
@@ -59,4 +55,4 @@ sudo gem install bropages
 # TODO: CLI tool for installing things from app store
 # https://github.com/herrbischoff/awesome-osx-command-line
 
-cp $HOME/dotfiles/p10k/.p10k.zsh $HOME
+cp $DOTFILES_FOLDER/p10k/.p10k.zsh $HOME
