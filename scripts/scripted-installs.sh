2b010d8ef09f303cf854abba1332095c010948e6
Deepak Naidu <devdnn@users.noreply.github.com>
2023-08-25 16:39:25 -0700

    Update scripted-installs.sh

--- scripts/scripted-installs.sh
+++ scripts/scripted-installs.sh
@@ -8,21 +8,21 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # install fonts
 cd ~/Library/Fonts
 mkdir "Segoe UI MDL2" && cd $_
-curl -O "https://github.com/devdnn/macos-setup/blob/main/fonts/Segoe UI MDL2/Segoe UI Bold Italic.ttf"
-curl -O "https://github.com/devdnn/macos-setup/blob/main/fonts/Segoe UI MDL2/Segoe UI Bold.ttf"
-curl -O "https://github.com/devdnn/macos-setup/blob/main/fonts/Segoe UI MDL2/Segoe UI Italic.ttf"
-curl -O "https://github.com/devdnn/macos-setup/blob/main/fonts/Segoe UI MDL2/Segoe UI Regular.ttf"
-curl -O "https://github.com/devdnn/macos-setup/blob/main/fonts/Segoe UI MDL2/Segoe UI Semibold.ttf"
-curl -O "https://github.com/devdnn/macos-setup/blob/main/fonts/Segoe UI MDL2/Segoe UI Semilight.ttf"
-curl -O "https://github.com/devdnn/macos-setup/blob/main/fonts/Segoe UI MDL2/Segoe UI.ttf"
+curl -O "https://raw.githubusercontent.com/devdnn/macos-setup/main/fonts/Segoe UI MDL2/Segoe UI Bold Italic.ttf"
+curl -O "https://raw.githubusercontent.com/devdnn/macos-setup/main/fonts/Segoe UI MDL2/Segoe UI Bold.ttf"
+curl -O "https://raw.githubusercontent.com/devdnn/macos-setup/main/fonts/Segoe UI MDL2/Segoe UI Italic.ttf"
+curl -O "https://raw.githubusercontent.com/devdnn/macos-setup/main/fonts/Segoe UI MDL2/Segoe UI Regular.ttf"
+curl -O "https://raw.githubusercontent.com/devdnn/macos-setup/main/fonts/Segoe UI MDL2/Segoe UI Semibold.ttf"
+curl -O "https://raw.githubusercontent.com/devdnn/macos-setup/main/fonts/Segoe UI MDL2/Segoe UI Semilight.ttf"
+curl -O "https://raw.githubusercontent.com/devdnn/macos-setup/main/fonts/Segoe UI MDL2/Segoe UI.ttf"
 
 cd ~/Library/Fonts
 mkdir "Omnes" && cd $_
-curl -O "https://github.com/devdnn/macos-setup/blob/main/fonts/Omnes/Omnes-Bold.otf"
-curl -O "https://github.com/devdnn/macos-setup/blob/main/fonts/Omnes/Omnes-Regular.otf"
-curl -O "https://github.com/devdnn/macos-setup/blob/main/fonts/Omnes/OmnesExtraLight.otf"
-curl -O "https://github.com/devdnn/macos-setup/blob/main/fonts/Omnes/OmnesLight.otf"
-curl -O "https://github.com/devdnn/macos-setup/blob/main/fonts/Omnes/OmnesMedium.otf"
-curl -O "https://github.com/devdnn/macos-setup/blob/main/fonts/Omnes/OmnesSemiBold.otf"
+curl -O "https://raw.githubusercontent.com/devdnn/macos-setup/main/fonts/Omnes/Omnes-Bold.otf"
+curl -O "https://raw.githubusercontent.com/devdnn/macos-setup/main/fonts/Omnes/Omnes-Regular.otf"
+curl -O "https://raw.githubusercontent.com/devdnn/macos-setup/main/fonts/Omnes/OmnesExtraLight.otf"
+curl -O "https://raw.githubusercontent.com/devdnn/macos-setup/main/fonts/Omnes/OmnesLight.otf"
+curl -O "https://raw.githubusercontent.com/devdnn/macos-setup/main/fonts/Omnes/OmnesMedium.otf"
+curl -O "https://raw.githubusercontent.com/devdnn/macos-setup/main/fonts/Omnes/OmnesSemiBold.otf"
 
 cd "$CURRENT_DIR"
